3054cd9de69c09f5bb276eb37524ac8a8b8258ce
nikoladjurein <nikdjurein@gmail.com>
2025-09-05 16:14:11 +0200

    uradjen doker zadorm-service i model

---
+++ dorm-service/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Dorm service is running ✅")
+	})
+
+	port := "8081"
+	log.Printf("Dorm service is listening on port %s...", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
+}
